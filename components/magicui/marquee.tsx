909930f1b5b93c8f1bdff32a6b294673d3031b2a
Sudhanshu <digitallynext2017@gmail.com>
2025-05-20 15:59:17 +0530

    The fix

--- components/magicui/marquee.tsx
+++ components/magicui/marquee.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
+// import { motion } from "framer-motion";
+import {  useState } from "react";
 
 interface MarqueeProps {
   /**
