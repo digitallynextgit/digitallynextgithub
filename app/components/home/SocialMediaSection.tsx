b02043eef65a9bb2d3bdc33044e40f6a02f0e7c8
DigitallyNext <digitallynext2017@gmail.com>
2025-06-27 14:00:52 +0530

    The new

--- app/components/home/SocialMediaSection.tsx
+++ app/components/home/SocialMediaSection.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import ImgCarousel from "@/components/ImgCarousel"
-import CircularGallery from "@/components/home/CircularGallery";
 
 const SocialMediaSection = () => {
   // Define gallery items using images from both gallery and flow directories
