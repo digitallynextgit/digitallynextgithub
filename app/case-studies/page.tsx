f75cabfe1b82436861e5dfcc80948ec0f2d6999c
Sudhanshu Srivastava <digitallynext2017@gmail.com>
2025-07-18 13:04:56 +0530

    refactor(case-studies): remove unused Link import from page component

--- app/case-studies/page.tsx
+++ app/case-studies/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import Link from 'next/link';
+// import Link from 'next/link';
 // import Image from 'next/image';
 import { caseStudies } from '@/app/data/case-studies';
 import { CaseStudyIndustry, CaseStudySector, CaseStudyRegion } from '@/types/case-study';
